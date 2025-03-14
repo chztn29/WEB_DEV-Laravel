fbf371c989850e3464f7bb5d3ca30847930ce965
chztn <chazteen29@gmail.com>
2025-03-14 23:43:07 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+// use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// View
+Route::get('/', [StudentsController::class, 'myWelcomeView'])->name(name: 'std.myWelcomeView');
+
+// Create
+Route::post('/create', [StudentsController::class, 'createNewStd'])->name(name: 'std.createNew');
+
+Route::put('/students/{student}', [StudentsController::class, 'update'])->name(name: 'std.update');
+Route::delete('/students/{student}', [StudentsController::class, 'delete'])->name(name: 'std.delete');
